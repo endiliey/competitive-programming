f1e2d2e672a16127ea33d29ab76afd5134ebdb57
Endiliey <endiliey@gmail.com>
2017-02-24 02:35:42 +0800

    fix soln B

--- Codeforces/sherlock-and-his-girlfriend.cpp
+++ Codeforces/sherlock-and-his-girlfriend.cpp
@@ -1,45 +1,40 @@
 #include <bits/stdc++.h>
-#define N 100010
+#define N 100110
+#define ll long long
 using namespace std;
 
 int main()
 {
-	int n;
+	ll n;
 	cin >> n;
-	// sieve or eratostehens
-	int a[N];
-	int b[N];
-	int i;
+	// sieve method
+	ll a[N];
+	ll b[N];
+	ll i;
 	for (i = 2; i <= n + 1; i++)
+	{
 		a[i] = 1;
+		b[i] = 1;
+	}
+
+	ll k = 1;
+
 	for (i = 2; i <= n + 1; i++)
+	{
 		if (a[i])
 		{
-			for (int j = i; j * i <= n + 1; j++)
+			for (ll j = i; j * i <= n + 1; j++)
 			{
 				a[i * j] = 0;
+				b[i * j] = b[i] + 1;
+				k = max(b[i * j], k);
 			}
 		}
-	
-	int k = INT_MIN;
-	// end of sieve
-	for (int x = 2; x <= n + 1; ++x)
-	{	
-		b[x] = 1;
-		if (a[x] == 0) // if not prime, find the prime divisor 
-		{
-			for (int y = 2; y < x; ++y)
-			{
-				if (a[y] == 1 && (a[x] % a[y] == 0)) // check if a number is prime divisor
-					b[x] = b[y] + 1;	
-			}
-		}
-		k = max(b[x], k); 
 	}
-	
+	// end of sieve
 	
 	cout << k << endl;
-	for (int cntr = 2; cntr <= n + 1; cntr++)
+	for (ll cntr = 2; cntr <= n + 1; cntr++)
 	{
 		cout << b[cntr] << " ";
 	}
