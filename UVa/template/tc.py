8c893029219df2ebc88e3095e926f28f89348b98
endiliey <endiliey@gmail.com>
2017-05-18 21:57:15 +0800

    Rename dir

---
+++ UVa/template/tc.py
@@ -0,0 +1,6 @@
+import random
+n = 50
+print n
+for x in xrange(n):
+    int rotate = random.randint(-1e9, 1e9)
+
---
+++ UVa/template/template.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef vector<int> vi; 
+typedef vector<vi> vvi; 
+typedef pair<int,int> ii; 
+typedef long long ll;
+#define sz(a) int((a).size()) 
+#define PB push_back 
+#define MP make_pair
+#define F first
+#define S second
+#define all(c) (c).begin(),(c).end() 
+#define present(c,x) ((c).find(x) != (c).end()) 
+
+int main()
+{
+    
+    return 0;
+}
